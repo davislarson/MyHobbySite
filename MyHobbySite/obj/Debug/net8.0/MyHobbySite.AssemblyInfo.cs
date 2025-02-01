6bc4b2c61579257f73a0acd5b9a5de24d304d9ea
Davis Larson <davislarson01@gmail.com>
2025-01-31 18:17:13 -0700

    some stuff

--- MyHobbySite/obj/Debug/net8.0/MyHobbySite.AssemblyInfo.cs
+++ MyHobbySite/obj/Debug/net8.0/MyHobbySite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyHobbySite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac276eb2873543f7265a3ede5670c2740d44c74c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyHobbySite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyHobbySite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
