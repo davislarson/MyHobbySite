4606fd0db2120d477f92713b20e327b141b20e60
Davis Larson <davislarson01@gmail.com>
2025-02-05 13:47:46 -0700

    all the files

--- MyHobbySite/wwwroot/js/site.js
+++ MyHobbySite/wwwroot/js/site.js
@@ -1,4 +1,17 @@
 ﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
+$(document).ready(function() {
+    $("#calculate").click(function() {
+        let hours = $("#hours").val();
+        let rate = 25.5; // Fixed hourly rate
 
-// Write your JavaScript code.
+        if (hours <= 0 || isNaN(hours)) {
+            alert("Please enter a valid positive number of hours.");
+            return;
+        }
+
+        let total = hours * rate;
+        $("#total").val("$" + total.toFixed(2));
+    });
+});
+
